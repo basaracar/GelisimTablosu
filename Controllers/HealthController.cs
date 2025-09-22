0d56a1885b43e7630a1348d7a10faa1faf287910
başar ACAROĞLU <basaracaroglu@hotmail.com>
2025-09-22 18:24:03 +0300

    Add health check endpoint

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+// Controllers/HealthController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace GelisimTablosu.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "healthy" });
+        }
+    }
+}
